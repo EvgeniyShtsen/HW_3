7b4ab30a8db8706e91db6c9eb5b982e42e4543f1
Eugene Shtsen <105601785+EvgeniyShtsen@users.noreply.github.com>
2022-09-18 01:51:12 +0300

    1
    1

--- Assets/TouchMove.cs
+++ Assets/TouchMove.cs
@@ -2,31 +2,25 @@ using UnityEngine;
 
 public class TouchMove : MonoBehaviour
 {
-    public Vector2 mouseVect;
-    public Vector2 sumVect;
-    public bool needImpulse;
-    private Vector2 startPos;
-
     void Update()
     {
         if (Input.touchCount > 0)
-        {
-            if (Input.touches[0].phase == TouchPhase.Began)
-                startPos = Input.touches[0].position; 
-            if (Input.touches[0].phase == TouchPhase.Ended)
-            { 
-                sumVect = startPos - Input.touches[0].position;
-            }
-        }
+            Swipe();
     }
-
-    void FixedUpdate()
+    void Swipe()
     {
-        if (needImpulse)
+        Vector2 delta = Input.GetTouch(0).deltaPosition;
+
+        if ((Mathf.Abs(delta.x)) > (Mathf.Abs(delta.y)))
         {
-            Vector3 forceVect = new Vector3(sumVect.x, sumVect.y);
-            GetComponent<Rigidbody2D>().AddForce(-forceVect, ForceMode2D.Impulse);
-            needImpulse = false;
+            if (delta.x > 0)
+            {
+                transform.Rotate(0, 5f, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -5f, 0);
+            }
         }
     }
 }
